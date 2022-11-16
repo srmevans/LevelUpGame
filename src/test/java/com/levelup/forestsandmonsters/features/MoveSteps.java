7c274ab99ee7d66fc1a74f165d2303fc9511c902
rijoxavier@yahoo.co.in <rijoxavier@yahoo.co.in>
2022-11-16 23:45:34 +0000

    Rijo : MoveSteps.java added

---
+++ src/test/java/com/levelup/forestsandmonsters/features/MoveSteps.java
@@ -0,0 +1,11 @@
+package com.levelup.forestsandmonsters.features;
+
+import com.levelup.forestsandmonsters.GameController;
+
+import java.awt.Point;
+public class MoveSteps {
+    GameController gc;
+    int startX, startY, endX, endY;
+    GameController.DIRECTION direction;
+    Point currentPosition;
+}
