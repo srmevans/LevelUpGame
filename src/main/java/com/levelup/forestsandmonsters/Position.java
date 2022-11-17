50363ee8f6f1463563a9ccff6eda7550f51fcb37
Stephanie Evans <stephanie.r.evans@gmail.com>
2022-11-17 23:45:57 +0000

    Stephanie & Manuel: Positions class and tests add

---
+++ src/main/java/com/levelup/forestsandmonsters/Position.java
@@ -0,0 +1,11 @@
+package com.levelup.forestsandmonsters;
+import java.awt.Point;
+
+public class Position {
+    Point coordinates; 
+
+    public Position(int xCoordinates, int yCoordinates) {
+
+        coordinates = new Point(xCoordinates, yCoordinates);
+    }
+}
---
+++ src/test/java/com/levelup/forestsandmonsters/PositionTest.java
@@ -0,0 +1,13 @@
+package com.levelup.forestsandmonsters;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PositionTest {
+    @Test
+public void initializationPositionTest()
+    {
+        Position testObj = new Position(1, 0);
+        assertEquals(0, testObj.coordinates.x);
+    }
+}
