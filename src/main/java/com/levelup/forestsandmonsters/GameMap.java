749f7190a4723b668f54a410312c85ea26e0c414
rijoxavier@yahoo.co.in <rijoxavier@yahoo.co.in>
2022-11-18 16:55:20 +0000

    Rijo: files postionc changed

---
+++ src/main/java/com/levelup/forestsandmonsters/GameMap.java
@@ -0,0 +1,28 @@
+package com.levelup.forestsandmonsters;
+
+import com.levelup.forestsandmonsters.GameController.DIRECTION;
+import java.awt.Point;
+
+public class GameMap {
+Position [][] positions;
+public Position startingPosition = new Position(0, 0);
+public int numPostions = 100;
+
+    public Position [][] getPositions(){
+    return null;
+    }
+
+    public Position calculatePosition(Position startPosition, DIRECTION direction){
+        return null;
+    }
+
+    private boolean isPositionValid(Point positionsCoordinates){
+        return false;
+    }
+
+    public int getTotalPositions(){
+        return 0;
+    }
+}
+
+
---
+++ src/test/java/com/levelup/forestsandmonsters/FakeGameMap.java
@@ -0,0 +1,9 @@
+package com.levelup.forestsandmonsters;
+
+public class FakeGameMap extends GameMap{
+
+    public static final Object STUBBED_NEW_POSITION = null;
+    public Object directionProvided;
+    public Object startingPosition;
+
+}
